e20a44bc7ad04f575d855b7f2648fa17873227bc
parvathisuresh <parvathisuresh0220@gmail.com>
2024-06-26 20:37:33 +0530

    Newspaperapp

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 import logo from './logo.svg';
 import './App.css';
 import Addnewspaper from './components/Addnewspaper';
+import SearchNewspaper from './components/SearchNewspaper';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ViewSubscriberpage from './components/ViewSubscriberpage';
 
 function App() {
   return (
-    <div>
-      <Addnewspaper/>
-       
-
-    </div>
+    
+    <BrowserRouter>
+    <Routes>
+      <Route path="/"element={<Addnewspaper/>}/>
+      <Route path="/view"element={<ViewSubscriberpage/>}/>
+      <Route path="/search"element={<SearchNewspaper/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/ViewSubscriberpage.jsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+const ViewSubscriberpage = () => {
+  return (
+    <div>
+      <div className="container">
+        <div className="row">
+            <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+            <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">SI num</th>
+      <th scope="col">Name</th>
+      
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+    <tr>
+      <th scope="row">1</th>
+      <td>gouri</td>
+      /</tr>
+      <tr>
+      <th scope="row">2</th>
+      <td>gouri</td>
+      /</tr>
+      <tr>
+      <th scope="row">3</th>
+      <td>gouri</td>
+      /</tr>
+      <tr>
+      <th scope="row">4</th>
+      <td>gouri</td>
+      /</tr>
+      <tr>
+      <th scope="row">5</th>
+      <td>gouri</td>
+      /</tr>
+      <tr>
+      <th scope="row">6</th>
+      <td>gouri</td>
+      /</tr>
+      <tr>
+      <th scope="row">7</th>
+      <td>gouri</td>
+      /</tr>
+      <tr>
+      <th scope="row">8</th>
+      <td>gouri</td>
+      /</tr>
+      <tr>
+      <th scope="row">9</th>
+      <td>gouri</td>
+      /</tr>
+      <tr>
+      <th scope="row">10</th>
+      <td>gouri</td>
+      /</tr>
+  </tbody>
+</table>
+            </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default ViewSubscriberpage
